ecf02f1aef4206820edbb74da12524a7a02812c9
Vladimir Pouzanov <farcaller@gmail.com>
2022-07-29 18:07:30 +0100

    Track the awake characters.

--- lib/stores/store.dart
+++ lib/stores/store.dart
@@ -1,3 +1,17 @@
+// Copyright 2022 Vladimir Pouzanov <farcaller@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import 'dart:async';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -151,6 +165,8 @@ abstract class _RootStore with Store {
 
       _authResolved.complete();
 
+      await client.subscribe('core.chars', 'awake');
+
       authenticated = true;
       if (!reconnecting) {
         _loginStreamController.sink.add(LoginEvent.loggedIn);
@@ -189,5 +205,12 @@ abstract class _RootStore with Store {
     final newPlayer = await client.call('core', 'getPlayer');
     logger.info('got player $newPlayer');
     player = newPlayer;
+  }
+
+  List<ResModel> get awakeChars {
+    final awakeCi = client.get('core.chars.awake');
+    if (awakeCi == null) return [];
+    final awake = awakeCi.item as ResModel;
+    return awake.toJson().values.map((e) => e as ResModel).toList();
   }
 }
